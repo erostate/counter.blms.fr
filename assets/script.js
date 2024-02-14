b5a85f63054c00ff21004cedce2620c2afc49bcf
Erostate <erostate55@gmail.com>
2024-02-14 02:19:03 +0100

    :sparkless: first fonctionality

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
 /*
-    GLOBAL JS for project RAND.BLMS.FR
+    GLOBAL JS for project COUNTER.BLMS.FR
 
     Made by: Erostate
-    Date: 2024-02-09
+    Date: 2024-02-13
     Version: 1.0.0
-    URI: https://rand.blms.fr
+    URI: https://counter.blms.fr
 
     License: BLMS
     License URI: https://blms.fr/license
@@ -23,7 +23,19 @@ function addRowName() {
     var newLine = document.createElement('span');
     newLine.className = 'list-name-wrapper';
     newLine.id = 'name' + newNameNumber + '_container';
-    newLine.innerHTML = '<input type="text" id="name' + newNameNumber + '" placeholder="Name ' + newNameNumber + '"><i class="fa-solid fa-xmark" onclick="removeRowName(' + newNameNumber + ')"></i>';
+    newLine.innerHTML = `
+    <span>
+        <input type="text" id="name${newNameNumber}" placeholder="Name ${newNameNumber}">
+        <button id="name${newNameNumber}_button" onclick="saveName(${newNameNumber})">SAVE</button>
+    </span>
+    <span id="name${newNameNumber}_scoreContainer">
+        <h1 class="name-no-save">0</h1>
+    </span>
+    <span>
+        <i class="fa-solid fa-trash name-no-save"></i>
+        <i onclick="removeRowName(${newNameNumber})" class="fa-solid fa-xmark"></i>
+    </span>
+    `;
 
     // Append the new input field
     list.insertBefore(newLine, document.getElementById('addName'));
@@ -63,191 +75,67 @@ function removeRowName(nameNumber) {
     list.removeChild(lineToRemove);
 }
 
-// Generate the team randomly
-function generateTeam() {
-    generateErrorModal('deactive');
-
-    openModal('modalGenerationTeam');
-
-    // Get the list of names
-    var list = document.getElementById('nameList');
-    var names = list.getElementsByTagName('input');
-    var totalCountName = names.length;
-
-    // Get the number of teams
-    var teamNumber = document.getElementById('numberOfTeam').value;
-
-    // Check if there are enough non-empty inputs, otherwise display an error message
-    var countEmptyCells = 0;
-    var listNames = [];
-    for (cell of names) {
-        console.log(cell.value);
-        if (cell.value == '') {
-            countEmptyCells++
-        } else {
-            listNames.push(cell.value);
-        }
-    }
-    if (countEmptyCells > teamNumber) {
-        generateErrorModal('active');
-        return;
+function saveName(nameNumber) {
+    var deleteRow = ""
+    if (nameNumber >= 3) {
+        deleteRow = `<i onclick="removeRowName(${nameNumber})" class="fa-solid fa-xmark"></i>`;
     }
 
-    // Mix the list of names
-    let shuffledList = [...listNames];
-    for (let i = shuffledList.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [shuffledList[i], shuffledList[j]] = [shuffledList[j], shuffledList[i]];
-    }
+    // Get the name
+    var name = document.getElementById('name' + nameNumber).value;
 
-    // Create the teams
-    var teamList = [];
-    for (var i = 0; i < teamNumber; i++) {
-        teamList.push(shuffledList.splice(0, Math.ceil(shuffledList.length / (teamNumber - i))));
-    }
-
-    // Display the teams
-    const teamListContainer = document.getElementById('teamList');
-    teamListContainer.innerHTML = '';
-    for (team of teamList) {
-        const teamContainer = document.createElement('div');
-        teamContainer.className = 'team';
-        teamContainer.id = 'team' + (teamList.indexOf(team) + 1);
-        teamContainer.innerHTML = '<h3>Team ' + (teamList.indexOf(team) + 1) + '</h3><hr>';
-        const teamMembers = document.createElement('ul');
-        teamMembers.id = 'team' + (teamList.indexOf(team) + 1) + 'List';
-        for (member of team) {
-            teamMembers.innerHTML += '<li>' + member + '</li>';
-        }
-        teamContainer.appendChild(teamMembers);
-        teamListContainer.appendChild(teamContainer);
-    }
-
-    // Save the team list in a cookie
-    const jsonData = {
-        "id": generateRandomId(),
-        "nbTeam": teamNumber,
-        "nbName": totalCountName,
-        "date": getCurrentDate(),
-        "history": teamList.map((team, index) => ({
-            "name": "Team " + (index + 1),
-            "members": team.map(member => ({ "name": member }))
-        }))
-    };
-
-    const jsonString = JSON.stringify(jsonData);
-
-    // Set the cookie
-    document.cookie = `teamData=${encodeURIComponent(jsonString)}; expires=${new Date(2025, 0, 1).toUTCString()}; path=/`;
-}
-
-function generateRandomId() {
-    return Math.random().toString(36).substr(2, 10);
-}
-
-function getCurrentDate() {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = (today.getMonth() + 1).toString().padStart(2, '0');
-    const day = today.getDate().toString().padStart(2, '0');
-    return `${year}-${month}-${day}`;
-}
-
-
-function getTeamDataFromCookie() {
-    const cookieValue = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('teamData='))
-        ?.split('=')[1];
-
-    if (cookieValue) {
-        return JSON.parse(decodeURIComponent(cookieValue));
-    }
-
-    return null;
-}
-function getTeamDataById(id) {
-    const cookieValue = document.cookie
-        .split('; ')
-        .find(row => row.startsWith('teamData='))
-        ?.split('=')[1];
-
-    if (cookieValue) {
-        const allTeamData = Array.isArray(JSON.parse(decodeURIComponent(cookieValue))) ? JSON.parse(decodeURIComponent(cookieValue)) : [];
-        const teamData = allTeamData.find(data => data.id === id);
-
-        return teamData || null;
-    }
-
-    return null;
-}
-
-function displayHistory() {
-    const historySection = document.getElementById('historyGeneration');
-    const teamData = getTeamDataFromCookie();
-
-    if (teamData) {
-        const previewDiv = document.createElement('div');
-        previewDiv.className = 'preview';
-        previewDiv.onclick = function() {
-            openHistory(teamData.id);
-        };
-
-        const teamsParagraph = document.createElement('p');
-        teamsParagraph.innerHTML = `<b>${teamData.nbTeam}</b> teams`;
-
-        const peopleParagraph = document.createElement('p');
-        peopleParagraph.innerHTML = `<b>${teamData.nbName}</b> people`;
-
-        const dateParagraph = document.createElement('p');
-        dateParagraph.innerHTML = `<b>${teamData.date}</b>`;
-
-        previewDiv.appendChild(teamsParagraph);
-        previewDiv.appendChild(peopleParagraph);
-        previewDiv.appendChild(dateParagraph);
-
-        historySection.appendChild(previewDiv);
+    // Check if the name is not empty
+    if (name != '') {
+        // Change the input field to a span
+        var nameContainer = document.getElementById('name' + nameNumber + '_container');
+        var valueNameInput = document.getElementById('name' + nameNumber).value;
+        nameContainer.classList.remove('name-no-save');
+        nameContainer.innerHTML = `
+        <span>
+            <input disabled type="text" id="name${nameNumber}" placeholder="Name ${nameNumber}" value="${valueNameInput}">
+            <button id="name${nameNumber}_button" onclick="editName(${nameNumber})">EDIT</button>
+        </span>
+        <span id="name${nameNumber}_scoreContainer">
+            <button onclick="changeScore(${nameNumber}, 'remove')" class="big-btn"><b><i class="fa-solid fa-minus"></i>1</b></button>
+            <h1>0</h1>
+            <button onclick="changeScore(${nameNumber}, 'add')" class="big-btn"><b><i class="fa-solid fa-plus"></i>1</b></button>
+        </span>
+        <span>
+            <i class="fa-solid fa-trash"></i>
+            ${deleteRow}
+        </span>
+        `;
+    } else {
+        document.getElementById('name' + nameNumber).focus();
     }
 }
 
-function openHistory(id) {
-    // Récupérer les données du cookie correspondant à l'ID
-    const teamData = getTeamDataById(id);
-
-    if (teamData) {
-        // Ouvrir la modal "modalHistory"
-        openModal('modalHistory');
-
-        // Remplir les informations dans la modal
-        const teamListContainer = document.getElementById('teamListHistory');
-        teamListContainer.innerHTML = '';
-
-        for (const team of teamData.history) {
-            const teamContainer = document.createElement('div');
-            teamContainer.className = 'team';
-            teamContainer.id = `team${teamData.history.indexOf(team) + 1}`;
-            teamContainer.innerHTML = `<h3>Team ${teamData.history.indexOf(team) + 1}</h3><hr>`;
-            
-            const teamMembers = document.createElement('ul');
-            teamMembers.id = `team${teamData.history.indexOf(team) + 1}List`;
-
-            for (const member of team.members) {
-                teamMembers.innerHTML += `<li>${member.name}</li>`;
-            }
-
-            teamContainer.appendChild(teamMembers);
-            teamListContainer.appendChild(teamContainer);
-        }
+function editName(nameNumber) {
+    var deleteRow = ""
+    if (nameNumber >= 3) {
+        deleteRow = `<i onclick="removeRowName(${nameNumber})" class="fa-solid fa-xmark"></i>`;
     }
+
+    // Enable the input field
+    var nameContainer = document.getElementById('name' + nameNumber + '_container');
+    var valueNameInput = document.getElementById('name' + nameNumber).value;
+    nameContainer.classList.add('name-no-save');
+    nameContainer.innerHTML = `
+    <span>
+        <input type="text" id="name${nameNumber}" placeholder="Name ${nameNumber}" value="${valueNameInput}">
+        <button id="name${nameNumber}_button" onclick="saveName(${nameNumber})">SAVE</button>
+    </span>
+    <span id="name${nameNumber}_scoreContainer">
+        <h1>0</h1>
+    </span>
+    <span>
+        <i class="fa-solid fa-trash"></i>
+        ${deleteRow}
+    </span>
+    `;
 }
 
-displayHistory();
 
-
-function deleteCookie() {
-    document.cookie = 'teamData=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-    location.reload();
-}
 
 
 // --- MODAL --- //
